a1c7369ff4fa6aac66800aaffbf06f40b2cfac2f
Saxbeck <saxbeck.patrick@gmail.com>
2022-10-19 00:20:24 +0200

    Added embedded player, but commented out

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,18 @@ export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="60vmin" pointerEvents="none" />
+        <ColorModeSwitcher justifySelf="flex-end" visibility={"hidden"} />
+        <VStack spacing={50}>
+          <Logo h="70vmin" pointerEvents="none" />
         </VStack>
+          {/*
+          <VStack>
+            <iframe src="http://146.190.227.164/public/acc_radio/embed?volume=100?&theme=light"
+                allowTransparency={true}
+                style={{width: "80%", border:"0"}}>
+            </iframe>
+          </VStack>
+          */}
       </Grid>
     </Box>
   </ChakraProvider>
